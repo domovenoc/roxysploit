6dfc581c296bda17da58fb05c1c819c7ee4320d1
domovenoc <74230887+domovenoc@users.noreply.github.com>
2020-11-11 08:58:26 +0300

    Update update.py

--- update.py
+++ update.py
@@ -4,7 +4,7 @@ import subprocess
 import os
 
 def update():
-	print "[*]Updating roxysploit framework, Please Wait ..."
+	print("[*]Updating roxysploit framework, Please Wait ...")
 	try:
 		subprocess.Popen("cd /tmp;wget http://xl65ae7epcwr2z58.rf.gd/rsf.zip ;unzip rsf.zip; rm rsf.zip", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).wait()
 	except Exception, e:
